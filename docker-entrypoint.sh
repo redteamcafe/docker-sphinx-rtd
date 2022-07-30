2d76827a155deb002d52a2e5caf3f868fe58a86c
Red Team Cafe <109925943+redteamcafe@users.noreply.github.com>
2022-07-30 07:34:31 -0400

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,9 +3,9 @@
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
 #NOTE: Start NGINX service
-exec service nginx start
+service nginx start
 
 #NOTE: Start autosphinx service
-exec service autosphinx start
+service autosphinx start
 
 /bin/bash
